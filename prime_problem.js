e5908229bb843ece101505e5c4e0470c645daa75
Khushi Gupta <khushigupta.of@gmail.com>
2022-09-20 22:18:19 +0530

    Write Logic

---
+++ prime_problem.js
@@ -0,0 +1,12 @@
+let n = 13 
+function prime(n){
+    if(n <= 1)
+    return false;
+    for(let i=2;i<n;i++){
+         return false
+    }
+    return true;
+}
+
+let num = prime(13)
+console.log(num)
